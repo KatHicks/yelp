9e3069dd378a29cc557e34c78398f1193773c7ed
Kat Hicks <kmhicks92@gmail.com>
2017-02-24 10:03:00 +0000

    Passed all but one test

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :restuarant
-  
+  belongs_to :restaurant
+
   validates :rating, inclusion: (1..5)
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -21,7 +21,6 @@ feature 'reviewing' do
      click_button 'Leave Review'
 
      expect(current_path).to eq '/restaurants'
-     click_link 'KFC'
      expect(page).to have_content('so so')
   end
 
